ae997b117a58a8a6a4e74715714af8bd1b8e06f7
Abhishek Sharma <sharma.abhishek.it@gmail.com>
2015-02-06 12:53:51 +0530

    Adding abstraction for flexbile communication across modules

---
+++ models.go
@@ -0,0 +1,35 @@
+package fcompute
+
+import (
+  "time"
+)
+
+const shortTimeFormat = "2006-01-02"
+
+type FDataDate time.Time
+
+func (t *FDataDate) UnmarshalJSON(b []byte) error {
+  v, _ := time.Parse(shortTimeFormat, string(b[1:len(b)-1]))
+  *t = FDataDate(v)
+  return nil
+}
+
+func (t1 FDataDate) DaysSince(t2 FDataDate) int {
+  this, that := time.Time(t1), time.Time(t2)
+  return int(that.Sub(this).Hours() / 24) + 1
+}
+
+type UserPrefs struct {
+  StartDate            FDataDate `json:"start_date"`
+  EndDate              FDataDate `json:"end_date"`
+  RebalanceFrequency   int       `json:"frequency"`
+  Weights              []float64 `json:"weights"`
+}
+
+type ComputedFData struct {
+  OriginalData        [][]float64
+  DailyAssetValue     [][]float64
+  NetDailyAssetValue  []float64
+  ProductNames        []string
+  SectorToProducts    map[string] []string
+}
---
+++ models_test.go
@@ -0,0 +1,46 @@
+package fcompute
+
+import (
+  "testing"
+  "encoding/json"
+  "time"
+  "reflect"
+)
+func TestJSONUnmarshallingUserPrefs(t *testing.T) {
+  var input = `
+  {
+    "start_date": "2014-12-30",
+    "end_date":   "2014-12-31",
+    "frequency":  7,
+    "weights":    [3.1, 4.1, 5.1]
+  }
+  `
+  expected := UserPrefs{
+    StartDate: FDataDate(time.Date(2014, time.December, 30, 0,0,0,0,time.UTC)),
+    EndDate:   FDataDate(time.Date(2014, time.December, 31, 0,0,0,0,time.UTC)),
+    RebalanceFrequency: 7,
+    Weights:   []float64{3.1, 4.1, 5.1},
+  }
+  got := UserPrefs{}
+  json.Unmarshal([]byte(input), &got)
+
+
+  if !(reflect.DeepEqual(got,expected)) {
+    t.Error("Unmarshalled UserPrefs are different. They are equal on weights, start, end, freq",
+      reflect.DeepEqual(got.Weights,expected.Weights),
+      got.StartDate == expected.StartDate,
+      got.EndDate   == expected.EndDate,
+      got.RebalanceFrequency == expected.RebalanceFrequency,
+    )
+  }
+}
+
+func TestUserPrefsDateDifference(t *testing.T) {
+  t1 := FDataDate(time.Date(2014, time.December, 30, 0,0,0,0,time.UTC))
+  t2 := FDataDate(time.Date(2014, time.December, 31, 0,0,0,0,time.UTC))
+  expected := 2
+
+  if t1.DaysSince(t2) != expected {
+    t.Error("Difference in days is not expected", t1.DaysSince(t2), expected)
+  }
+}
