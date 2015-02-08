a5128ca2fb469f4e64eae4b22ddd78839f794fd5
Abhishek Sharma <sharma.abhishek.it@gmail.com>
2015-02-08 17:22:25 +0530

    moving to a more friendly code structure

---
+++ lib/report_generators.go
@@ -0,0 +1,21 @@
+package fcompute
+
+type ProfitNLoss struct {
+  report_name string
+  data        []float64
+}
+
+type NetReturn struct {
+  report_name string
+  data        float64
+}
+
+type AnnualizedReturn struct {
+  report_name string
+  data        float64
+}
+
+type MaximumDropdown struct {
+  report_name string
+  data        float64
+}
