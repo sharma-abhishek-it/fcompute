7df5967296223d01468e71f4f8e0e72b0b65d39e
Abhishek Sharma <sharma.abhishek.it@gmail.com>
2015-02-10 21:26:00 +0530

    Adding test cases to honor UserPrefs in PreComute calculations

--- lib/compute_algos_test_precompute.go
+++ lib/compute_algos_test_precompute.go
@@ -1,6 +1,9 @@
 package fcompute
 
-import "testing"
+import (
+  "testing"
+  "os"
+)
 
 func testing_data()(ComputedFData, UserPrefs) {
   data  := ComputedFData {
@@ -36,6 +39,56 @@ func TestPreComputeWithRebalancedEndDay(t *testing.T) {
 
 func TestPreComputeWithUnbalancedEndDay(t *testing.T) {
   data,prefs := testing_data()
+  prefs.RebalanceFrequency = 2
+
+  expected_daily_asset_value,expected_net_asset := [3]int {21,21,62}, 105
+
+  data.PreCompute(prefs)
+
+  for i,p_data := range data.DailyAssetValue  {
+    if int(p_data[2]) != expected_daily_asset_value[i] {
+      t.Errorf("Expected output mismatched for product #%d | Expected = %d, Got = %f",
+      i, expected_daily_asset_value[i], data.DailyAssetValue[i][2])
+    }
+  }
+
+  if int(data.NetDailyAssetValue[2]) != expected_net_asset {
+    t.Errorf("Expected output mismatched for net_daily_asset | Expected = %d, Got = %f",
+    expected_net_asset, data.NetDailyAssetValue[2])
+  }
+}
+
+func TestPreCompute_With_Custom_Starting_Ending_Dates_Data_Length(t *testing.T) {
+  data,prefs := testing_data()
+  data.OriginalData = [][]float64 {{3,5,4,6,7},{2,4,3,3,8},{5,6,4,7,9}}
+
+  os.Setenv(ENV_START_DATE_KEY, "2014-01-22")
+  os.Setenv(ENV_END_DATE_KEY, "2014-01-26")
+
+  prefs.fillDates("2014-01-23", "2014-01-25")
+
+  prefs.RebalanceFrequency = 2
+
+  expected_length := 2
+
+  data.PreCompute(prefs)
+
+  if len(data.NetDailyAssetValue) != expected_length {
+    t.Errorf("Expected length mismatched for net_daily_assets | Expected = %d, Got = %f",
+    expected_length, len(data.NetDailyAssetValue))
+  }
+}
+
+
+func TestPreCompute_With_Custom_Starting_Ending_Dates_Exact_Data(t *testing.T) {
+  data,prefs := testing_data()
+  data.OriginalData = [][]float64 {{3,5,4,6,7},{2,4,3,3,8},{5,6,4,7,9}}
+
+  os.Setenv(ENV_START_DATE_KEY, "2014-01-22")
+  os.Setenv(ENV_END_DATE_KEY, "2014-01-26")
+
+  prefs.fillDates("2014-01-23", "2014-01-25")
+
   prefs.RebalanceFrequency = 2
 
   expected_daily_asset_value,expected_net_asset := [3]int {21,21,62}, 105
